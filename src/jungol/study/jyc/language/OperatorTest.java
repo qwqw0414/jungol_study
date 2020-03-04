c9011269560364f5f19904765b3c20b397697362
qwqw0414 <xzdi0414@gmail.com>
2020-03-05 04:11:37 +0900

    Revert "20-03-05"
    This reverts commit 45f55f6b6e59d51ae2b9fef7d8e1b91ea82d7a2e.

--- src/jungol/study/jyc/language/OperatorTest.java
+++ src/jungol/study/jyc/language/OperatorTest.java
@@ -9,35 +9,7 @@ public class OperatorTest {
 	public static void main(String[] args) {
 		OperatorTest ot = new OperatorTest();
 //		ot.ex1();
-//		ot.ex2();
-//		ot.ex3();
-//		ot.ex4();
-		ot.ex5();
-	}
-	
-	private void ex5() {
-		int[] a = {150,35};
-		int[] b = {145, 35};
-		
-		System.out.println(a[0]>b[0] ? (a[1]>b[1] ? 1 : 0 ) : 0);
-	}
-	
-	private void ex4() {
-		int num1 = 10;
-		int num2 = 15;
-		
-		System.out.println(++num1 + "\t" + num2--);
-		System.out.println(num1 + "\t" + num2);
-	}
-	
-	private void ex3() {
-		int num1 = 20;
-		int num2 = 15;
-		
-		num1 += 5;
-		num2 *= 2;
-		
-		System.out.printf("width = %d\nlength = %d\narea = %d",num1,num2,num1*num2);
+		ot.ex2();
 	}
 	
 	private void ex2() {
@@ -52,11 +24,13 @@ public class OperatorTest {
 	private void ex1() {
 		int[] score = {70,95,63,100};
 		int sum = 0;
+		int cnt = 0;
 		
 		for(int num : score) {
 			sum += num;
+			cnt++;
 		}
 		
-		System.out.printf("sum %d\navg %d",sum,sum/score.length);
+		System.out.printf("sum %d\navg %d",sum,sum/cnt);
 	}
 }
