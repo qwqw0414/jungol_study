8476fc0dfcc1ed0f4ece66de529b8d914a5e6553
최현규 <chlgusrbv59@gmail.com>
2020-03-13 20:47:39 +0900

    테스트

--- src/jungol/study/chk/Run.java
+++ src/jungol/study/chk/Run.java
@@ -3,5 +3,20 @@ package jungol.study.chk;
 public class Run {
 	public static void main(String[] args) {
 		System.out.println("댄당");
+		System.out.println("키보드 바꿧지로롱");
+		
+		for(int i=1; i<=9; i++) {
+			System.out.println(i+"단");
+			for(int j=1; j<=9; j++) {
+				System.out.println(i+"*"+j+"="+i*j);
+			}
+		}
+		
+		System.out.println("키보드 환장하누~");
+		
+		
+		
+		
+		
 	}
 }
